dab9548ae27b4b5419b90b78d12f31e5c3133369
kamalesh0703 <kamaleshponmani0703@gmail.com>
2023-09-19 22:04:37 +0530

    inizal change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import {LeftMenu} from './Components/LeftMenu';
+import { MainContainer } from './Components/MainContainer';
+import { RightMenu } from './Components/RightMenu';
 
 function App() {
   return (
     <div className="App">
+      <LeftMenu/>
+      <MainContainer/>
+      <RightMenu/>
+      <div className='background'></div>
     </div>
   );
 }
