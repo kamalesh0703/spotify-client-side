8cdaf91a554b79ec8b2fe66b8520857a0c115143
kamalesh0703 <kamaleshponmani0703@gmail.com>
2023-09-20 21:53:35 +0530

    day 3 commit

--- src/Components/LeftMenu.jsx
+++ src/Components/LeftMenu.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import '../Styles/LeftMenu.css';
+import { FaSpotify,FaEllipsisH } from 'react-icons/fa';
+import {BiSearchAlt} from 'react-icons/bi';
 
 function LeftMenu() {
   return (
-    <div>LeftMenu</div>
+    <div className='leftMenu'>
+      <div className="logoContainer">
+        <i><FaSpotify/></i>
+        <h2>Spotify</h2>
+        <i><FaEllipsisH/></i>
+      </div>
+      <div className="searchBox">
+        <input type="text" placeholder='Search...'  className='searchinput'/>
+        <i className='searchIcon'><BiSearchAlt/></i>
+      </div>
+    </div>
   )
 }
 
